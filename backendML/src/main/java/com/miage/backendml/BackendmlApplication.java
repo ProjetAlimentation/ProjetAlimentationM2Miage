cf22da57fdb11cfabae7f2114e43a679df6f007b
Théo DECARTE <theo.decarte@gmail.com>
2021-03-11 10:49:12 +0100

    Initial commit

---
+++ backendML/src/main/java/com/miage/backendml/BackendmlApplication.java
@@ -0,0 +1,13 @@
+package com.miage.backendml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendmlApplication.class, args);
+    }
+
+}
---
+++ backendML/src/test/java/com/miage/backendml/BackendmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.backendml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
