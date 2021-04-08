f534fe259458be1a8b2e8edddd774855f9f5ee7b
Théo DECARTE <theo.decarte@gmail.com>
2021-04-08 10:25:17 +0200

    nav

---
+++ frontendAngular/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import {BackendService} from '../backend.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+
+  constructor(private breakpointObserver: BreakpointObserver,
+              private backendService: BackendService, private router: Router) {}
+
+
+
+  logout(): void {
+    console.log('Logout');
+    localStorage.setItem('isLoggedIn', 'false');
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
