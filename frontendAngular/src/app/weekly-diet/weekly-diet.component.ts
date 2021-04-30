de041b59ae21bea2d0daa4f7cde77209f1f7b990
raouf7777 <haroun.raouf@hotmail.fr>
2021-04-30 14:34:14 +0200

    #10 fixed back and front completed

--- frontendAngular/src/app/weekly-diet/weekly-diet.component.ts
+++ frontendAngular/src/app/weekly-diet/weekly-diet.component.ts
@@ -49,8 +49,6 @@ export class WeeklyDietComponent implements OnInit {
 
   updateDishInDietList(dishKey, dishIndex, profile?): void{
     const username = localStorage.getItem('token');
-
-    console.log("sds " + profile);
     this.backendService.getRegeneratedDish(username, dishKey, dishIndex, profile).subscribe( (dishMap) => {
       this.dishMap = dishMap;
       console.log(dishMap);
