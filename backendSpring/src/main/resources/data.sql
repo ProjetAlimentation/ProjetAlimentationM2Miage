ef35d774279c91e2d274c39328a5dbae8583a201
raouf7777 <haroun.raouf@hotmail.fr>
2021-05-06 19:03:16 +0200

    small changes

--- backendSpring/src/main/resources/data.sql
+++ backendSpring/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO USERS (username, password) values ('admin','password');
 INSERT INTO PRODUCT_CART (id,username) values (1,'admin');
-INSERT INTO PROFILE values (1, null, 'LACTOSE_FREE', null, 'VEGETARIAN', null, 'admin');
+INSERT INTO PROFILE values (1, null, null, null, null, null, 'admin');
